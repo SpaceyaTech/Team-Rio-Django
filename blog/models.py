80503a8ae5d4ad83d860a7446e52f32aa0ce8b97
JimmyTron <oduorjames@hotmail.com>
2023-01-06 03:08:27 +0300

    Refactor Pre-dev
    correcting errors on the pre-dev branch
    addition of new db to avoid migration conflict
    
    	modified:   CMS/settings.py
    	modified:   blog/admin.py
    	modified:   blog/models.py
    	new file:   db-pre-dev.sqlite3

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,19 @@
 
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
-
-class Account(models.Model):
-    user = models.OneToOneField(User, related_name='account', on_delete=models.CASCADE )
-    bio = models.CharField(max_length=30, blank=True, null=True)
-    created_at = models.DateTimeField(auto_created=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user
-
+from accounts.models import *
+ 
+class Post(models.Model):
+    author = models.ForeignKey(Account, on_delete=models.CASCADE)
 class Comment(models.Model):
-    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    author = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment_author = models.ForeignKey(Account, on_delete=models.CASCADE)
     text = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved_comment = models.BooleanField(default=False)
+    publish_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approved_comment = True
+        self.publish_comment = True
         self.save()
 
     def __str__(self):
